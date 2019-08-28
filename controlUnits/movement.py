32854e94902ee1ab55c60599ca190814c69acaa3
master <1053782496@qq.com>
2019-08-28 23:06:05 +0800

    2019-8-28 对日志的更新

---
+++ controlUnits/movement.py
@@ -0,0 +1,15 @@
+#
+
+
+import gpio
+import time 
+
+
+class Movement():
+    def __init__(self):
+        self.Goleft = GoLeft
+        self.GoRight = GoRight
+        self.GoForwad = GoForward
+        self.GoBack = goBack
+        self.signal = signal
+        self.channel = channel
